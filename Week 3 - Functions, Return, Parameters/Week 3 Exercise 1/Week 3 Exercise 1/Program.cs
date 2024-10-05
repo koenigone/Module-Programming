8be179b320a59b8b358da45745553653c3345d02
Mohammad Mohammad <mm6dd.mh@gmail.com>
2024-10-05 19:20:27 +0100

    Added Week 3 exercise, completed warm up tasks

---
+++ Week 3 - Functions, Return, Parameters/Week 3 Exercise 1/Week 3 Exercise 1/Program.cs
@@ -0,0 +1,62 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // takes user input and allows user to enter a string
+        int ReadInt(string userAge)
+        {
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            return userInput;
+        }
+
+        int age = ReadInt("Enter your age");
+
+        Console.WriteLine(age);
+
+        Console.WriteLine(); // space
+
+        // ask the user for a number and return it's square
+        int Square(int n) 
+        {
+            return n * n;
+        }
+
+        Console.WriteLine("Enter a number to get it's square");
+        Console.Write("Square of: ");
+        int userSquare = Convert.ToInt32(Console.ReadLine());
+        int squared = Square(userSquare);
+
+        Console.WriteLine($"{userSquare} squared is: {squared}");
+
+        Console.WriteLine(); // space
+
+        bool IsOdd(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+
+        Console.WriteLine("Enter a number to see if it's an even or odd number");
+        Console.Write("My number: ");
+        int userNumEO = Convert.ToInt32(Console.ReadLine());
+        string userNumEOResault = "";
+
+        if (IsOdd(userNumEO)) 
+        {
+            userNumEOResault = "even";
+        } else
+        {
+            userNumEOResault = "odd";
+        }
+
+        Console.WriteLine($"Your number is: {userNumEOResault}");
+
+        Console.ReadKey();
+    }
+}
