fef8d75ccd75526558500fa642727a54ac6dcfbc
koenigone <mm6dd.mh@gmail.com>
2024-10-08 10:52:30 +0100

    initialized week 3 tougher task, half solved

---
+++ Week 3 - Functions, Return, Parameters/Week 3 Exercise 3/Week 3 Exercise 3/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Rectangle r = new Rectangle();
+        r.topLeft = new Point();
+        Point p = new Point();
+
+        bool result = Inside(r, p);
+        Console.WriteLine(result);
+
+
+
+        bool Inside(Rectangle r, Point p)
+        {
+            if (p.x > r.width)
+            {
+                return false;
+            } 
+            else if (p.y > p.y) 
+            { 
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+    }
+}
+
+class Point
+{
+    public float x;
+    public float y;
+}
+
+class Rectangle
+{
+    public Point topLeft;
+    public float width;
+    public float height;
+}
