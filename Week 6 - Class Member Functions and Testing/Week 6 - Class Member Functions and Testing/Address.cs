cc9ef3a0368d6ac0a1ab6feb261238f72d442c62
Mohammad Mohammad <mm6dd.mh@gmail.com>
2024-10-31 12:50:33 +0000

    added task 3, not completed

---
+++ Week 6 - Class Member Functions and Testing/Week 6 - Class Member Functions and Testing/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressClass
+{
+    internal class Address
+    {
+        public string building;
+        public string town;
+        public string city;
+        public string county;
+
+        void SetAddress(string address)
+        {
+
+        }
+    }
+}
