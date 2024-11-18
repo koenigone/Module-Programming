c0f6fb0bb9efa34c14e0d2faab52da6cb1b8af90
koenigone <mm6dd.mh@gmail.com>
2024-11-18 14:07:01 +0000

    started week 8 exercise - not completed

---
+++ Week 9 - Graphical User Interface (GUI)/Exercise 1/Exercise 1/Recipe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    internal class Recipe
+    {
+        private string recipeName;
+        List<string> recipeInstructions = new List<string>();
+        List<string> recipeIngredients = new List<string>();
+        private string recipePrepTime;
+        private string recipeCookTime;
+        private string recipePeople;
+        private bool veggie;
+
+        string RecipeName(string name)
+        {
+            recipeName = name;
+            return recipeName;
+        }
+        void Instrucions(string listItem)
+        {
+            recipeInstructions.Add(listItem);
+        }
+        void Ingredients(string listItem)
+        {
+            recipeIngredients.Add(listItem);
+        }
+        
+    }
+}
